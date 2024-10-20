543696f0b55185a670405f0a1133f643354db57e
冷瑜 <lengyu@reiyus-MacBook-Pro.local>
2024-10-21 04:44:42 +0800

    success to get raw url list

--- src/functions/scanTheEconomist.ts
+++ src/functions/scanTheEconomist.ts
@@ -1,8 +1,13 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
+import { url } from "inspector";
+import { JSDOM } from 'jsdom';
+const pLimit = require('p-limit');
+
+const limit = pLimit(10);
 
 export async function scanTheEconomist(myTimer: Timer, context: InvocationContext): Promise<void> {
     context.log('Timer function processed request.');
-    doScanTheEconomist(context);
+    await doScanTheEconomist(context);
 }
 
 // generate book class, include the book title, displayTitle, book version and url, version need to be number type. And displayTitle need to be title_version pattern
@@ -30,47 +35,69 @@ async function doScanTheEconomist(context: InvocationContext) {
     const html = await response.text();
 
     // Parse html to get all the economist subfolder (class id = 'folder-row-0')
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
+    const doc = new JSDOM(html).window.document;
     // query all item if class id match the pattern folder-row-number
     const economistFolders = doc.querySelectorAll('[id^=folder-row-]');
     const economistFolderAItems = Array.from(economistFolders).map(folder => folder.querySelector('a'));
-    // exclude item if titles not start with te_ and get the href
-    const economistFolderUrls = economistFolderAItems.filter(item => item.textContent.startsWith('te_')).map(item => item.href);
+    // exclude item if titles not start with te_ and get the href and convert to real url
+    const economistFolderUrls = economistFolderAItems.filter(item => item.textContent.startsWith('te_')).map(item => new URL(item.href, targetUrl))
+    //const economistFolderUrls = economistFolderAItems.filter(item => item.textContent.startsWith('te_')).map(item => item.href);
+
+    // using context.log to log all economistFolderUrls, as following format 'href'
+    economistFolderUrls.forEach(url => context.log(url));
+
+
 
     // open all of the economistFolderUrls and get html
-    const economistFolderHtmls = await Promise.all(economistFolderUrls.map(url => fetch(url).then(response => response.text())));
+    // using plimit to limit Promise.all to 2 concurrent fetches
+    //const economistFolderHtmls = await Promise.all(economistFolderUrls.map(url => fetch(url).then(response => response.text())));
+    const economistFolderHtmls = await Promise.all(economistFolderUrls.map(url => limit(() => fetch(url).then(response => response.text()))));
 
-    // Get book url from economistFolderHtmls, it exist in a tag with title include epub. The result need to be a object list which include the href link and the text for each a tag
+    // Get book url from economistFolderHtmls, it exist in a tag with title include epub and class include Link-Primary. The result need to be a object list which include the url link and the text for each a tag
     const economistBookUrls = economistFolderHtmls.map(html => {
-        const doc = parser.parseFromString(html, 'text/html');
+        const doc = new JSDOM(html).window.document;
         const aTags = Array.from(doc.querySelectorAll('a'));
-        return aTags.filter(a => a.title.includes('epub')).map(a => ({ href: a.href, text: a.textContent }));
+        return aTags.filter(a => a.title.includes('epub') && a.classList.contains('Link--primary')).map(a => ({ href: a.href, text: a.textContent, url: new URL(a.href, targetUrl) }));
     }).flat();
+
+    context.log('Success to get the root folder page and parsed it.');
+    // log all of the economistBookUrls, as following format 'text url'
+    economistBookUrls.forEach(url => context.log(`text: ${url.text} url: ${url.url}`));
+    context.log('ended');
 
     // open all of the economistBookUrls and get html. Save html to new object list. each object should include the html result and the textContent for each item of economistBookUrls
-    const economistBookRawHTMLs = await Promise.all(economistBookUrls.map(url => fetch(url.href).then(response => response.text().then(html => ({ html, text: url.text })))));
-
+    // using plimit to limit Promise.all to 2 concurrent fetches
+    //const economistBookRawHTMLs = await Promise.all(economistBookUrls.map(url => fetch(url.href).then(response => response.text().then(html => ({ html, text: url.text })))));
+    const economistBookRawHTMLs = await Promise.all(economistBookUrls.map(url => limit(() => fetch(url.url).then(response => response.text().then(html => ({ html, text: url.text }))))));
     
     const economistBookRawLinks = economistBookRawHTMLs.map(html => {
-        const doc = parser.parseFromString(html.html, 'text/html');
+        const doc = new JSDOM(html.html).window.document;
         const aTags = Array.from(doc.querySelectorAll('a'));
-        return aTags.filter(a => a.querySelector('span')?.textContent === 'Raw').map(a => ({ href: a.href, text: html.text }));
+        return aTags.filter(a => a.querySelector('span')?.textContent === 'Raw').map(a => ({ href: a.href, text: html.text, url: new URL(a.href, targetUrl) }));
     }).flat();
 
-    // construct book object from economistBookRawUrls. Title = the economist. text pattern = 'te_YYYY.MM.DD'. version = YYYYMMDD. url = href
+    context.log('Success to get the book page and parsed it.');
+
+    // construct book object from economistBookRawUrls. Title = the economist. text pattern = 'TheEconomist.YYYY.MM.DD.epub'. version = YYYYMMDD. url = href
+    const regex = /\b(\d{4}\.\d{2}\.\d{2})\b/;
     const economistBooks = economistBookRawLinks.map(item => {
         const title = 'The Economist';
-        const version = parseInt(item.text.split('_')[1].replace(/\./g, ''));
-        const url = item.href;
-        return new Book(title, version, url);
+        const match = item.text.match(regex);
+        const version = parseInt(match[1].split('.').join(''));
+        const url = item.url;
+        return new Book(title, version, url.toString());
     });
 
-    // using context log all economistBooks
-    economistBooks.forEach(book => context.log(book));
+    context.log('Found the following Economist books:');
+
+    // using context log all economistBooks. As the format 'title displayTitle version url'
+    economistBooks.forEach(book => {
+        context.log(`${book.title} ${book.displayTitle} ${book.version} ${book.url}`);
+    });
 }
 
 app.timer('scanTheEconomist', {
-    schedule: '0 * */1 * * *',
+    schedule: '0 * * */1 * *',
+    runOnStartup: true,
     handler: scanTheEconomist
 });
